a9ca037ecbc1bb02d812800bbe15f05f164e08c7
Ubuntu <ubuntu@ip-172-31-22-136.us-west-1.compute.internal>
2022-08-21 02:52:45 +0000

    edit:improved

--- project/main.py
+++ project/main.py
@@ -11,12 +11,13 @@ from pprint import pprint
 from typing import Union, List
 from pydantic import BaseModel
 from .S3 import upload_file, handle_upload_mp3, handle_upload_img
-import shutil, random, requests
+import shutil, random, requests, time
 from pathlib import Path
 from tempfile import NamedTemporaryFile
 from typing import Callable
 from sqlalchemy import inspect
 from pydub import AudioSegment
+from pydub.playback import play
 
 
 
@@ -118,39 +119,63 @@ async def all_users(db:Session = Depends(get_db)):
     return {'users': users}
 
 @app.post('/edit')
-async def get_record(ids : list[int], db: Session=Depends(get_db)):
+async def get_record(ids : list[int], filename: str, db: Session=Depends(get_db)):
     sounds = []
     
     for i in range(len(ids)):
 
         # 쿼리를 해서 url 주소를 받아 놓으면
         URL = db.query(models.Sources.link).filter(models.Sources.id == ids[i]).first()._asdict()['link']
-        filename = db.query(models.Sources.title).filter(models.Sources.id == ids[i]).first()._asdict()['title']
+        source_name = db.query(models.Sources.title).filter(models.Sources.id == ids[i]).first()._asdict()['title']
         # 거기서 response 얻어와서 byte 값 가져옴
         response = requests.get(URL)
 
         # 그걸 m4a 형태로 저장해 둠 
-        open(f"{filename}.m4a", 'wb').write(response.content)
+        open(f"{i}.m4a", 'wb').write(response.content)
 
         # wav로 전환
         # track = AudioSegment.from_file(f"./{filename}.m4a", foramt='m4a')
         # file = track.export(f"./{filename}.wav", format='wav')
 
-        subprocess.call(['ffmpeg', '-i', f'{filename}.m4a', f'{filename}.wav'])
-
-        # 다시 오디오 세그먼트로 wav 파일을 편집에 사용케 함. 
-        sound = AudioSegment.from_wav(f'{filename}.wav')
-        sounds.append(sound)
+        subprocess.call(['ffmpeg', '-i', f'{i}.m4a', f'{i}.wav'])
 
         # m4a 로 저장해뒀던 파일은 다시 초기화.
-        remove(f"{filename}.m4a")
+        remove(f'{i}.m4a')
     
-    overlapped = sound[0].overlay(sound[1], position=0)
-    overlapped = overlapped.overlay(sound[2], position=0)
     
-    file_handle = overlapped.export('output.wav', format="wav")
+    # 다시 오디오 세그먼트로 wav 파일을 편집에 사용케 함. 
+    global overlapped_0
+    overlapped_0 = AudioSegment.from_wav('0.wav')
+    
+    for i in range(1, len(ids)):
+
+        globals()['overlapped_{}'.format(i)] = globals()[f'overlapped_{i-1}'].overlay(AudioSegment.from_wav(f'{i}.wav'))
+        
+
+    # n이라고 가정
+    i=0
+    
+    # overlapped = sound[0].overlay(sound[1], position=0)
+    # overlapped = overlapped.overlay(sound[2], position=0)
+    
+    file_handle = overlapped_2.export(f'{filename}.wav', format="wav")
 
     # wav 파일도 지워야 하는데 어떡하지
     subprocess.call(['ffmpeg', '-i', f'{filename}.wav', f'{filename}.m4a'])
 
-    return FileResponse('output.m4a')
+    handle_upload_mp3(f'./{filename}.m4a', filename="audio/"+filename+".m4a")
+
+    # 여기도 아래 링크들 확인 필요
+    img_link = image[random.randint(0,len(image)-1)]
+    link = f"https://record777777.s3.ap-northeast-2.amazonaws.com/audio/{filename}.m4a"
+    new_source = models.Sources(title=filename,link=link, img_link=img_link)
+
+    db.add(new_source)
+    db.commit()
+    db.refresh(new_source)
+    remove('0.wav')
+    remove('1.wav')
+    remove('2.wav')
+    remove(f'{filename}.wav')
+    remove(f'{filename}.m4a')
+    return
